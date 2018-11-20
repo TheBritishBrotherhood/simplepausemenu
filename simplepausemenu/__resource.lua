fd11b3bd91b42eccff198608614c0ed7feb54ecc
TheBritishBrotherhood <TheBritishBrotherhood@users.noreply.github.com>
2018-11-20 16:03:44 +0100

    Add files via upload

---
+++ simplepausemenu/__resource.lua
@@ -0,0 +1,13 @@
+-- resource manifest
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- client scripts
+client_scripts {
+    'client/pause_menu.lua'
+}
+
+---DO NOT REDISTRIBUTE---
+
+---ANY ISSUES LEAVE A COMMENT ON GITHUB---
+
+---TheBritishBrotherhood---
